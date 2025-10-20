7e9e48fb484f5ad278f318a7001554a394095175
Marcos Bernal <mberurb751@g.educaand.es>
2025-10-20 11:59:19 +0200

    Configurado 20 de octubre

---
+++ src/main/java/com/iesvdm/demospth2025/DemoSpTh2025Application.java
@@ -0,0 +1,13 @@
+package com.iesvdm.demospth2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpTh2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpTh2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/iesvdm/demospth2025/DemoSpTh2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesvdm.demospth2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpTh2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
