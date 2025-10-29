df8324d2df3bdc0762a7fddcd1ef4abb6bcaf834
Marcos Bernal <mberurb751@g.educaand.es>
2025-10-29 12:53:15 +0100

    Modificacion en ClienteDAOImpl.java, Cliente.java y application.properties (29 de Octubre)

--- src/main/java/com/iesvdm/demospth2025/dao/ClienteDAOImpl.java
+++ src/main/java/com/iesvdm/demospth2025/dao/ClienteDAOImpl.java
@@ -3,6 +3,7 @@ package com.iesvdm.demospth2025.dao;
 import com.iesvdm.demospth2025.modelo.Cliente;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -27,12 +28,52 @@ public class ClienteDAOImpl implements ClienteDAO{
 
     @Override
     public List<Cliente> getAll() {
-        return List.of();
+
+        List<Cliente> listClientes = jdbcTemplate.query(""" 
+                        select *
+                        from cliente
+                        """,
+                //RowMapper ----v
+                (rs, rowNum) ->new Cliente(
+                                            rs.getInt("id"),
+                                            rs.getString("nombre"),
+                                            rs.getString("apellido1"),
+                                            rs.getString("apellido2"),
+                                            rs.getString("ciudad"),
+                                            rs.getInt("categoría")
+                )
+        );
+
+        log.info("Devueltos {} clientes", listClientes.size());
+
+        return listClientes;
     }
 
     @Override
     public Optional<Cliente> find(int id) {
-        return Optional.empty();
+
+        try{
+        Cliente cliente = jdbcTemplate.queryForObject(""" 
+                        select * from cliente
+                        where id = ?
+                        """,
+                (rs, rowNum)-> Cliente.builder()
+                        .id(rs.getInt("id"))
+                        .nombre(rs.getString("nombre"))
+                        .apellido1(rs.getString("apellido1"))
+                        .apellido2(rs.getString("apellido2"))
+                        .ciudad(rs.getString("ciudad"))
+                        .categoria(rs.getInt("categoría"))
+                        .build()
+                ,
+                id
+            );
+
+            return Optional.of(cliente);
+
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
--- src/test/java/com/iesvdm/demospth2025/DemoSpTh2025ApplicationTests.java
+++ src/test/java/com/iesvdm/demospth2025/DemoSpTh2025ApplicationTests.java
@@ -1,13 +1,53 @@
 package com.iesvdm.demospth2025;
 
+import com.iesvdm.demospth2025.dao.ClienteDAO;
+import com.iesvdm.demospth2025.modelo.Cliente;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class DemoSpTh2025ApplicationTests {
 
+    //Spring es un framework de Inyeccion de Dependencias e Inversion de Control
+
+    @Autowired
+    ClienteDAO  clienteDAO;
+
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void testGetAll(){
+        List <Cliente> list = clienteDAO.getAll();
+            list.forEach(System.out::println);
+    }
+
+    @Test
+    void testFindOk(){
+        Optional<Cliente> optionalCliente = clienteDAO.find(3);
+        if (optionalCliente.isPresent()){
+            System.out.println(optionalCliente.get());
+        } else {
+            //optionalCliente.isEmpty();
+            System.out.println("VACIO!!");
+        }
+    }
+
+    @Test
+    void testFindFail(){
+        Optional<Cliente> optionalCliente = clienteDAO.find(100);
+        if (optionalCliente.isPresent()){
+            System.out.println(optionalCliente.get());
+        } else {
+            //optionalCliente.isEmpty();
+            System.out.println("VACIO!!");
+        }
     }
 
 }
